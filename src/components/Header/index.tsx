49e87cfa76832546800d6c25f3811eb94088264c
lansen-git <109493673+lansen-git@users.noreply.github.com>
2022-07-21 22:26:41 +0800

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -127,7 +127,7 @@ const BalanceText = styled(Text)`
 `
 
 const NETWORK_LABELS: { [chainId in ChainId]: string | null } = {
-  [ChainId.MAINNET]: MAINNET,
+  [ChainId.MAINNET]: null,
   [ChainId.RINKEBY]: 'Rinkeby',
   [ChainId.ROPSTEN]: 'Ropsten',
   [ChainId.G脰RLI]: 'G枚rli',
